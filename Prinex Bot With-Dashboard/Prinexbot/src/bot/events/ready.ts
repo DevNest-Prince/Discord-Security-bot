f1a1f8d74ac80498399eedec3e50585a1efcd3ed
Prince Kothari <197196645+princekotharii@users.noreply.github.com>
2026-08-21 12:48:11 +0530

    deleted folder

--- Prinex Bot With-Dashboard/Prinexbot/src/bot/events/ready.ts
+++
@@ -1,9 +0,0 @@
-import { botClient } from '../client';
-import { logger } from '../../utils/logger';
-
-botClient.once('clientReady', () => {
-  if (botClient.user) {
-    logger.info(`Logged in and ready as ${botClient.user.tag} on ${botClient.guilds.cache.size} guilds.`);
-    botClient.user.setActivity('over community security', { type: 3 });
-  }
-});
